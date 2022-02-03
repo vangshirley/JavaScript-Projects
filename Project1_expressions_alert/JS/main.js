cd77409751832e353be5c09fb9b23cb953ac84cb
vangshirley <96396500+vangshirley@users.noreply.github.com>
2022-02-02 19:37:59 -0800

    Initial commit

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,22 @@
+document.write("She then told me, \"Stop procrastinating and do your homework, or you'll regret it.\"<br>\"I will after this episode,\" I responded."); //this is to continue the string and tells the computer it has a special character in it
+
+alert("Hello, World!"); //this presents a pop up when opening web page
+
+document.getElementById('yourElemId').style.color = 'red';
+
+document.write("<br><br>\"Be who you are and say what you feel,"                //this concatenates a string value       
++ " because those who mind don't matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
+
+var B ="<Br><Br>Concatenated" + " String" //this assigns a concatenate string value to a variable
+document.write(B)
+
+var Family = "The Arezzenis", Dad = "Jeremiah", Mom = "Hermione", //this assigns multiple variables in one statment
+    Daughter = "Penny", Son = "Zorro";
+
+document.write( Dad)
+
+var myBikeName = "Yamaha", myCarName="BMW", name="John David"; //this assigns multiple variables in one statment
+document.write(myBikeName)
+
+document.write(3+3);
---
+++ README.md
@@ -0,0 +1,5 @@
+# JavaScript-Projects
+Hello, my name is Shirley and I am UX/UI Designer student at the Tech Academy in Portland, OR.
+Here, you will find my Javascript projects from various assignments. 
+
+Please check out my HTML and CSS Portfolio for more.
