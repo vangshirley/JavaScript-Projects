ac54bef47a0305d82affd585fcc1847ffd3170d3
vangshirley <96396500+vangshirley@users.noreply.github.com>
2022-02-08 19:46:51 -0800

    Operators

---
+++ Project6_ternary__operators_constructors/JS/main.js
@@ -0,0 +1,67 @@
+function Ride_Function() { //age old enough or too young
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+}
+
+function Vote_Function() { //input age old enough or too young to vote
+    var Age, Can_vote;
+    Age = document.getElementById("Age").value;
+    Can_vote = (Age < 18) ? "You are not old enough to vote.":"You can vote!";
+    document.getElementById("Vote").innerHTML = Can_vote;
+}
+
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML =
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + 
+    " manufactured in " + Erik.Vehicle_Year;
+}
+
+
+function Vehicle(Make, Model, Year, Color) { //New Keyword
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myVehicle() {
+    document.getElementById("New_And_This").innerHTML =
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + 
+    " manufactured in " + Erik.Vehicle_Year;
+}
+
+//Assign variable to reserved word
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age     = age;
+    this.eyeColor = eye;
+    const myFather = new Person("John", "Doe", 50, "blue"); //create person object
+}
+function myFather() {
+    document.getElementById("demo").innerHTML =
+    "My father is " + myFather.age + "."; 
+}
+
+function count_Function() {
+    document.getElementById("Nested_Function").innerHTML = Count ();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
+}
